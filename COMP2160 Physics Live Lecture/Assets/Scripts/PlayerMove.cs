e246e4757d54648739346248d016705ada0f6ea5
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2026-09-23 18:25:43 +1000

    feature[gameplay]: Implement gravity on player.

---
+++ COMP2160 Physics Live Lecture/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,78 @@
+/**
+ * 
+ * Author: Malcolm Ryan
+ * Version: 1.0
+ * For Unity Version: 6000.0.53f1
+ */
+
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove : MonoBehaviour
+{
+
+#region Parameters
+    [SerializeField, Unit(Units.MetersPerSecond)] private float maxFallSpeed = -10;
+#endregion 
+
+#region Connected Objects
+#endregion
+
+#region Components
+    private Rigidbody2D rigidbody;
+#endregion
+
+#region State
+#endregion
+
+#region Properties
+#endregion
+
+#region Events
+#endregion
+
+#region Init & Destroy
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+#endregion 
+
+#region Update
+    void Update()
+    {
+    }
+#endregion
+
+#region FixedUpdate
+    void FixedUpdate()
+    {
+        Debug.Log($"[PlayerMove.FixedUpdate] rigidbody.linearVelocity.y = {rigidbody.linearVelocity.y}");
+
+        if (rigidbody.linearVelocity.y <= maxFallSpeed)
+        {
+            rigidbody.gravityScale = 0;
+
+            Vector2 velocity = rigidbody.linearVelocity;
+            velocity.y = maxFallSpeed;
+            rigidbody.linearVelocity = velocity;
+        }
+    }
+#endregion
+
+#region Gizmos
+    void OnDrawGizmos()
+    {
+        if (rigidbody == null)
+        {
+            return;
+        }
+
+        Handles.color = Color.white;
+        Handles.Label(transform.position, $"v = {rigidbody.linearVelocity.y}");
+
+    }
+#endregion
+}
